e90a16a1f202b24b7637bc71a1bd051c7b9f68fc
staFF6773 <108166164+staFF6773@users.noreply.github.com>
2023-09-24 03:51:01 -0600

    Update MainCommand.java

--- src/main/java/staff/commands/MainCommand.java
+++ src/main/java/staff/commands/MainCommand.java
@@ -6,9 +6,15 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import staff.main.Epicplugin;
+import staff.utils.MessageUtils;
 
 public class MainCommand implements CommandExecutor {
-    @Override
+
+    private Epicplugin plugin;
+
+    public MainCommand(Epicplugin plugin){
+        this.plugin = plugin;
+    }
     public boolean onCommand(CommandSender sender, Command command, String alias, String[] args) {
         if (!(sender instanceof Player)){
             //consola
@@ -18,9 +24,61 @@ public class MainCommand implements CommandExecutor {
         }
 
         //jugador
-        sender.sendMessage(ChatColor.translateAlternateColorCodes(
-                '&', Epicplugin.prefix+"You have just used the command &7/epicplugin."));
+        Player player = (Player) sender;
+
+
+        //Argumentos
+        if(args.length >= 1){
+            if(args[0].equalsIgnoreCase("get")){
+                //epicplugin get
+                subCommandGet(sender, args);
+            }else{
+                help(sender);
+            }
+        }else{
+            //epicplugin
+            help(sender);
+        }
+
 
         return true;
+    }
+
+    public void help(CommandSender sender){
+        sender.sendMessage(MessageUtils.getColoredMessage("&f&l-----------COMMANDS &b&lEPICPLUGIN&f&l-----------"));
+        sender.sendMessage(MessageUtils.getColoredMessage("&7- /epicplugin:heal"));
+        sender.sendMessage(MessageUtils.getColoredMessage("&7- /epicplugin get <author/version>"));
+        sender.sendMessage(MessageUtils.getColoredMessage("&f&l-----------COMMANDS &b&lEPICPLUGIN&f&l-----------"));
+    }
+
+    public void subCommandGet(CommandSender sender,String[] args){
+        if(!sender.hasPermission("epicplugin.commands.get")){
+            sender.sendMessage(MessageUtils.getColoredMessage(
+                    Epicplugin.prefix+" &c&lUPS &cSorry but you do not have permissions to execute this command."));
+            return;
+
+        }
+
+        if(args.length == 1){
+            sender.sendMessage(MessageUtils.getColoredMessage(Epicplugin.prefix+" &c&lUPS &cSorry but you have to use a valid value."));
+            sender.sendMessage(MessageUtils.getColoredMessage("&7/epicplugin get <author/version>."));
+            return;
+
+        }
+
+        if(args[1].equalsIgnoreCase("author")){
+            //epicplugin get author
+            sender.sendMessage(MessageUtils.getColoredMessage(
+                    Epicplugin.prefix+" &7The authors of the plugin are: &e"+plugin.getDescription().getAuthors()));
+
+        } else if (args[1].equalsIgnoreCase("version")) {
+            //epicplugin get version
+            sender.sendMessage(MessageUtils.getColoredMessage(
+                    Epicplugin.prefix+" &7The plugin version is: &e"+plugin.getDescription().getVersion()));
+        }else{
+            sender.sendMessage(MessageUtils.getColoredMessage(Epicplugin.prefix+" &c&lUPS &cSorry but you have to use a valid value."));
+            sender.sendMessage(MessageUtils.getColoredMessage("&7/epicplugin get <author/version>."));
+        }
+
     }
 }
