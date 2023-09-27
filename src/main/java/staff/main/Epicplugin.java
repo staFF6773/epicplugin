5816dafe9640e4c102c56d25a23f5b9f7fea1463
api-tsukasa <142162315+api-tsukasa@users.noreply.github.com>
2023-09-26 19:41:25 -0600

    Update Epicplugin.java

--- src/main/java/staff/main/Epicplugin.java
+++ src/main/java/staff/main/Epicplugin.java
@@ -5,8 +5,11 @@ import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 import staff.commands.MainCommand;
 import staff.commands.heal;
-import staff.evets.playerJoinQuit;
+import staff.commands.repair;
+import staff.manager.ConfigManager;
 import staff.utils.MessageUtils;
+
+import java.io.File;
 
 public final class Epicplugin extends JavaPlugin {
 
@@ -15,19 +18,24 @@ public final class Epicplugin extends JavaPlugin {
     ConsoleCommandSender mycmd = Bukkit.getConsoleSender();
     @Override //INICIO DE PLUGIN
     public void onEnable() {
+
+        ConfigManager.setupConfig(this);
+
         registerCommands();
 
-        getServer().getPluginManager().registerEvents(new playerJoinQuit(), this);
-        getCommand("heal").setExecutor(new heal());
         mycmd.sendMessage(MessageUtils.getColoredMessage(prefix+" &aThe plugin has started successfully! Version: "+version));
+        mycmd.sendMessage(MessageUtils.getColoredMessage(prefix+" &aThank you very much for using me :D &7ATT: not_staff"));
     }
 
     @Override//FINALIZAR PLUGIN
     public void onDisable(){
         mycmd.sendMessage(MessageUtils.getColoredMessage(prefix+" &aThe plugin has been successfully deactivated! Version: "+version));
+        mycmd.sendMessage(MessageUtils.getColoredMessage(prefix+" &aThank you very much for using me :D &7ATT: not_staff"));
     }
 
     public void registerCommands(){
-        this.getCommand("epicplugin").setExecutor(new MainCommand());
+        this.getCommand("repair").setExecutor(new repair(this));
+        this.getCommand("epicplugin").setExecutor(new MainCommand(this));
+        this.getCommand("heal").setExecutor(new heal());
     }
 }
