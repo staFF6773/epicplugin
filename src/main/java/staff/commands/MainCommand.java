ad9158cb7db8528e5073483b998c3c57a41e0631
staFF6773 <108166164+staFF6773@users.noreply.github.com>
2023-10-04 17:18:08 -0600

    Confirmar

--- src/main/java/staff/commands/MainCommand.java
+++ src/main/java/staff/commands/MainCommand.java
@@ -1,4 +1,4 @@
-package staff.commands;
+ackage staff.commands;
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -6,7 +6,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import staff.main.Epicplugin;
-import staff.utils.MessageUtils;
+import staff.utils.ChatUtils;
 
 public class MainCommand implements CommandExecutor {
 
@@ -44,42 +44,43 @@ public class MainCommand implements CommandExecutor {
         return true;
     }
 
-    //help command
     public void help(CommandSender sender){
-        sender.sendMessage(MessageUtils.getColoredMessage("&f&l-----------COMMANDS &b&lEPICPLUGIN&f&l-----------"));
-        sender.sendMessage(MessageUtils.getColoredMessage("&7- /epicplugin:heal"));
-        sender.sendMessage(MessageUtils.getColoredMessage("&7- /epicplugin:repair"));
-        sender.sendMessage(MessageUtils.getColoredMessage("&7- /epicplugin get <author/version>"));
-        sender.sendMessage(MessageUtils.getColoredMessage("&f&l-----------------------------------------"));
+        sender.sendMessage(ChatUtils.getColoredMessage("&f&l-----------COMMANDS &b&lEPICPLUGIN&f&l-----------"));
+        sender.sendMessage(ChatUtils.getColoredMessage("&7- /epicplugin:reload"));
+        sender.sendMessage(ChatUtils.getColoredMessage("&7- /epicplugin:heal"));
+        sender.sendMessage(ChatUtils.getColoredMessage("&7- /epicplugin:repair"));
+        sender.sendMessage(ChatUtils.getColoredMessage("&7- /epicplugin:pvp <on/off>"));
+        sender.sendMessage(ChatUtils.getColoredMessage("&7- /epicplugin get <author/version>"));
+        sender.sendMessage(ChatUtils.getColoredMessage("&f&l-----------------------------------------"));
     }
 
     public void subCommandGet(CommandSender sender,String[] args){
         if(!sender.hasPermission("epicplugin.commands.get")){
-            sender.sendMessage(MessageUtils.getColoredMessage(
+            sender.sendMessage(ChatUtils.getColoredMessage(
                     Epicplugin.prefix+" &c&lUPS &cSorry but you do not have permissions to execute this command."));
             return;
 
         }
 
         if(args.length == 1){
-            sender.sendMessage(MessageUtils.getColoredMessage(Epicplugin.prefix+" &c&lUPS &cSorry but you have to use a valid value."));
-            sender.sendMessage(MessageUtils.getColoredMessage("&7/epicplugin get <author/version>."));
+            sender.sendMessage(ChatUtils.getColoredMessage(Epicplugin.prefix+" &c&lUPS &cSorry but you have to use a valid value."));
+            sender.sendMessage(ChatUtils.getColoredMessage("&7/epicplugin get <author/version>."));
             return;
 
         }
 
         if(args[1].equalsIgnoreCase("author")){
             //epicplugin get author
-            sender.sendMessage(MessageUtils.getColoredMessage(
+            sender.sendMessage(ChatUtils.getColoredMessage(
                     Epicplugin.prefix+" &7The authors of the plugin are: &e"+plugin.getDescription().getAuthors()));
 
         } else if (args[1].equalsIgnoreCase("version")) {
             //epicplugin get version
-            sender.sendMessage(MessageUtils.getColoredMessage(
+            sender.sendMessage(ChatUtils.getColoredMessage(
                     Epicplugin.prefix+" &7The plugin version is: &e"+plugin.getDescription().getVersion()));
         }else{
-            sender.sendMessage(MessageUtils.getColoredMessage(Epicplugin.prefix+" &c&lUPS &cSorry but you have to use a valid value."));
-            sender.sendMessage(MessageUtils.getColoredMessage("&7/epicplugin get <author/version>."));
+            sender.sendMessage(ChatUtils.getColoredMessage(Epicplugin.prefix+" &c&lUPS &cSorry but you have to use a valid value."));
+            sender.sendMessage(ChatUtils.getColoredMessage("&7/epicplugin get <author/version>."));
         }
 
     }
--- src/main/java/staff/commands/heal.java
+++ src/main/java/staff/commands/heal.java
@@ -1,35 +1,47 @@
 package staff.commands;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
+import staff.main.Epicplugin;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class heal implements CommandExecutor {
     private final Map<Player, Long> cooldowns = new HashMap<>();
-    private final int cooldownTimeInSeconds = 600; // 10 minutos (600 segundos)
+    private int cooldownTimeInSeconds; // Tiempo de reutilización en segundos
+
+    public heal(Epicplugin plugin) {
+        // Carga la configuración desde el archivo config.yml al inicializar el comando
+        FileConfiguration config = plugin.getConfig();
+        cooldownTimeInSeconds = config.getInt("heal-cooldown", 600); // Valor predeterminado: 10 minutos (600 segundos)
+    }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!(sender instanceof Player)) {
-            sender.sendMessage("§f[§bEpicPlugin§f] §cThis command is only for players");
+            sender.sendMessage(ChatColor.translateAlternateColorCodes(
+                    '&', Epicplugin.prefix+" &cThis command is only for players"));
             return true;
         }
 
         Player player = (Player) sender;
 
         if (!player.hasPermission("epicplugin.heal")) {
-            player.sendMessage("§f[§bEpicPlugin§f] §c§lUPS §cIt seems that you do not have permissions to execute this command");
+            player.sendMessage(ChatColor.translateAlternateColorCodes(
+                    '&', Epicplugin.prefix+" &c&lUPS &cSorry but you do not have permissions to execute this command"));
             return true;
         }
 
         if (hasCooldown(player)) {
             long remainingTime = getCooldownTime(player);
-            player.sendMessage("§f[§bEpicPlugin§f] §cYou must wait " + remainingTime + " seconds before using this command again.");
+            player.sendMessage(ChatColor.translateAlternateColorCodes(
+                    '&', Epicplugin.prefix+" &cYou must wait " + remainingTime + " seconds before using this command again."));
             return true;
         }
 
@@ -38,7 +50,8 @@ public class heal implements CommandExecutor {
 
         player.setHealth(20);
         player.setFoodLevel(20);
-        player.sendMessage("§f[§bEpicPlugin§f] §aYou have been successfully cured");
+        player.sendMessage(ChatColor.translateAlternateColorCodes(
+                '&', Epicplugin.prefix+" &aYou have been successfully cured"));
 
         return true;
     }
